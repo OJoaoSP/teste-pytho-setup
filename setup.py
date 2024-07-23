782ae781640b528659eacc2902d0ccad1a4908e1
oponei <56514281+OJoaoSP@users.noreply.github.com>
2024-07-23 15:18:47 -0300

    teste

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name='printer',
-    version='0.7.0',
+    version='0.8.0',
     description='Descrição do seu pacote',
-    packages=find_packages(include=['printer_teste']),
+    packages=find_packages(where='printer_teste'),
     python_requires='>=3.9',
     setup_requires=['setuptools'],
     entry_points={
